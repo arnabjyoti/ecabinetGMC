1a805c27d724edd54371a52e9077d6f551950b7b
arnabjyoti <thakuriaarnabjyoti@gmail.com>
2025-12-30 22:47:12 +0530

    date changes

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: false,
   test:true,
-  BASE_URL: 'http://localhost:8900'
-  // BASE_URL: 'http://178.16.138.26:8600'
+  // BASE_URL: 'http://localhost:8900'
+  BASE_URL: 'https://backend.ecabinetmic.com'
 };
 
