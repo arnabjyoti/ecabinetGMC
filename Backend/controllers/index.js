d09711f073ce5ed0f04868c1293c78444bb0bd8d
Rupon Sarkar <ruponsarkar108@gmail.com>
2026-01-05 09:51:52 +0530

    dashboard

--- Backend/controllers/index.js
+++ Backend/controllers/index.js
@@ -1,8 +1,10 @@
 const AuthController = require('./AuthController');
 const IssuesController = require('./IssuesController');
 const sendMail = require('./sendMail');
+const analyticController = require('./analyticController');
 module.exports = {
 	AuthController,
 	IssuesController,
 	sendMail,
+	analyticController
 };
