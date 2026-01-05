7514cf2246ee8ad165caede74c4fed9f13658de6
arnabjyoti <thakuriaarnabjyoti@gmail.com>
2026-01-05 23:36:11 +0530

    dashboard updated

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: false,
   test:true,
-  // BASE_URL: 'http://localhost:8900'
-  BASE_URL: 'https://backend.ecabinetmic.com'
+  BASE_URL: 'http://localhost:8900'
+  // BASE_URL: 'https://backend.ecabinetmic.com'
 };
 
