fbf5f1c77bd9a4268214c876850ac9c82c2c3628
s0Fi98 <solmansofihussain@gmail.com>
2026-01-06 14:38:11 +0530

    Updated landing page footer

--- Frontend_backup/src - Copy/environments/environment.test.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-	test: true,
-	production: false,
-	BASE_URL: 'http://178.16.138.26:8600'
-};
--- Frontend_backup/src - Copy/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-
-export const environment = {
-  production: false,
-  test:true,
-  BASE_URL: 'http://localhost:8900'
-  // BASE_URL: 'http://178.16.138.26:8600'
-};
-
